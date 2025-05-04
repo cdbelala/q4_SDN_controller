24985e3ea3cecee144a5c2dce3f85d47ff868850
cdbelala <cdbelala@gmail.com>
2025-05-04 12:27:12 -0500

    added skeleton

---
+++ sdn_controller.py
@@ -0,0 +1,37 @@
+import os
+import sys
+import matplotlib.pyplot as plt
+import numpy as np
+import networkx as nx
+
+
+def main():
+    pass
+
+def generate_flow_tables():
+    pass
+
+def generate_topology_visual():
+    pass
+
+def generate_network_flows():
+    pass
+
+def generate_link_utilization():
+    pass
+
+def handle_link_failure():
+    pass
+
+def add_node():
+    pass
+
+def remove_node():
+    pass
+
+def simulate_link_failure():
+    pass
+
+def inject_traffic():
+    pass
+
