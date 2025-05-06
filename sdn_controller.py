0b6da34b8cee1bafb863f37c23539a9965ed07c1
cdbelala <cdbelala@gmail.com>
2025-05-06 10:56:21 -0500

    added to skeleton, added class for node object

--- sdn_controller.py
+++ sdn_controller.py
@@ -6,12 +6,27 @@ import networkx as nx
 import mininet as mn
 import ryu as ryu
 
+class node:
+    #all node attributes
+    node_id = node_id
+    type = kwargs.get('type', 'host')
+    ip = kwargs.get('ip', None)
+    mac = kwargs.get('mac', None)
+    port = kwargs.get('port', None)
+    links = kwargs.get('links', [])
+    active_flows = kwargs.get('active_flows', [])
+    link_utilization = kwargs.get('link_utilization', {})
+
+#-----------------------------------------------------------------
 #main entry point and flow control for program
 def main():
     pass
+#-----------------------------------------------------------------
 
+#-----------------------------------------------------------------
 #network topology and flow generation functions
 def create_node(node_id, **kwargs):
+
     pass
 
 def create_link(node1_id, node2_id, **kwargs):
@@ -28,7 +43,9 @@ def get_topology():
 
 def visualize_network_state(active_flows, link_utilization):
     pass
+#-----------------------------------------------------------------
 
+#-----------------------------------------------------------------
 #Pathfinding computation and routing
 def compute_shortest_path(src_node_id, dst_node_id, weight='latency'):
     pass
@@ -44,7 +61,9 @@ def delete_flow_rule(switch_id, match_criteria):
 
 def apply_routing_policy(flow):
     pass
+#-----------------------------------------------------------------
 
+#-----------------------------------------------------------------
 #Failure handling
 def handle_link_failure(node1_id, node2_id):
     pass
@@ -54,3 +73,7 @@ def get_link_utilization(node1_id, node2_id):
 
 def update_link_utilization(node1_id, node2_id, utilization):
     pass
+#-----------------------------------------------------------------
+
+if __name__ == "__main__":
+    main()
